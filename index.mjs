69347794a44a80ab45a3879ab5c9650d836ab6ca
Truong Nguyen <truong.nguyen@tymlez.com>
2023-03-29 09:53:45 +1100

    initial poc

---
+++ index.mjs
@@ -0,0 +1,382 @@
+import puppeteer from "puppeteer";
+import axios from "axios";
+let board_fen = "";
+let playIntervalId;
+let busy = false;
+let lastFen = null;
+let lastResult = null;
+
+function readCurrentBoardFen(page) {
+  return page.evaluate(() => {
+    const space = "-";
+    const index = new Array(64).fill(space);
+    let hasPiece = false;
+    for (var i = 1; i <= 8; i++)
+      for (var j = 1; j <= 8; j++) {
+        const el = document.querySelector(`.piece.square-${i}${j}`);
+        if (el) {
+          let piece = el
+            .getAttribute("class")
+            .split(" ")
+            .find((x) => x.length === 2 && ["w", "b"].includes(x[0]));
+
+          if (piece && piece[0] == "w") {
+            index[(j - 1) * 8 + i - 1] = piece[1].toUpperCase();
+            hasPiece = true;
+          }
+
+          if (piece && piece[0] == "b") {
+            hasPiece = true;
+            index[(j - 1) * 8 + i - 1] = piece[1].toLowerCase();
+          }
+        }
+      }
+    const row = [];
+    for (var i = 0; i < 8; i++) {
+      let fen = index.splice(0, 8).join("");
+      for (let t = 8; t >= 1; t--) {
+        fen = fen.replace(
+          new RegExp(new Array(t).fill(space).join(""), "ig"),
+          t.toString()
+        );
+      }
+      row.push(fen);
+    }
+    const board = row.reverse().join("/");
+    const blackMoves = document.querySelectorAll(".black.node");
+    const whiteMoves = document.querySelectorAll(".white.node");
+    const whoMoveNext = whiteMoves.length > blackMoves.length ? "b" : "w";
+
+    function kqStatus() {
+      const w = getCastleingStatus("w");
+      const b = getCastleingStatus("b");
+
+      if (w == "-" && b === "-") return "- -";
+      return w + b;
+    }
+    function getCastleingStatus(mover) {
+      let king = mover === "b" ? "king-black" : "white-king";
+      let query = mover === "b" ? ".black.node" : ".white.node";
+      const findMoves = [...document.querySelectorAll(query + " span")].map(
+        (x) => x.getAttribute("data-figurine")
+      );
+
+      const allMoves = [...document.querySelectorAll(query)].map((x) =>
+        x.textContent.trim()
+      );
+
+      const testMoves = [...allMoves, ...findMoves];
+      if (
+        testMoves.includes("K") ||
+        testMoves.includes("R") ||
+        testMoves.includes("O-O") ||
+        testMoves.includes("O-O-0")
+      ) {
+        return "-";
+      }
+
+      if (mover === "b") return "kq";
+      return "KQ";
+    }
+
+    //const kq = 'KQkq'
+    const kq = kqStatus();
+    return [`${board} ${whoMoveNext} ${kq} - 1 0`, hasPiece, whoMoveNext];
+  });
+}
+
+async function getBestMove(fen) {
+  if (fen === lastFen && lastResult) {
+    return lastResult;
+  }
+  const { data } = await axios.get(process.env.STOCKFISH_API, {
+    params: { fen },
+  });
+  lastResult = data.result.bestmove;
+  lastFen = fen;
+  return lastResult;
+}
+async function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+async function closeModalFirst(page, timeout = 5000) {
+  try {
+    await clickon(page, "[data-cy='modal-first-time-button']", timeout);
+  } catch {}
+}
+async function getCurrentFen(page) {
+  let fen = null;
+  try {
+    await clickon(page, "[data-cy='Download']", 1000);
+    await delay(100);
+    await page.waitForSelector(
+      ".share-menu-tab-pgn-section > .ui_v5-input-component",
+      { timeout: 1000 }
+    );
+
+    fen = await page.$eval(
+      ".share-menu-tab-pgn-section > .ui_v5-input-component",
+      (input) => {
+        return input.value;
+      }
+    );
+    await clickon(page, "[data-cy='share-menu-close']");
+  } catch (err) {
+    fen = await readCurrentBoardFen(page);
+    fen = fen[0];
+  }
+  console.log("fen", fen);
+  return fen;
+}
+
+async function ensureLogin(page) {
+  const username = process.env.username;
+  const password = process.env.password;
+  console.log("username", username);
+  await page.goto(
+    "https://www.chess.com/login_and_go?returnUrl=https://www.chess.com/"
+  );
+
+  console.log(page.url());
+
+  if (page.url() === "https://www.chess.com/home") {
+    console.log("user already logged in");
+    return;
+  }
+  await page.type("#username", username);
+  await page.type("#password", password);
+  await page.click("#login");
+  await delay(2000);
+}
+async function clickAt(page, square, fromSquare) {
+  // console.log("clickAt", square, "from square", fromSquare);
+  console.log("Play the move ", fromSquare, square);
+  const findEl = await page.$(fromSquare);
+
+  const className = await page.evaluate((el) => el.className, findEl);
+
+  // console.log("className", className);
+  const isBlack = className.includes("b") && !className.includes("wb");
+  const header = await page.$(".board");
+  const rect = await page.evaluate((header) => {
+    const { top, left, bottom, right } = header.getBoundingClientRect();
+    return { top, left, bottom, right };
+  }, header);
+
+  const board_x = rect.left;
+  const board_y = rect.top;
+  const width = 81;
+
+  const row = +square[1];
+  const col = +square[0];
+  // this work for play as white
+  let x = Math.round(board_x + (col - 1) * width + width / 2);
+  let y = Math.round(board_y + (8 - row) * width + width / 2);
+  if (isBlack) {
+    x = Math.round(board_x + (8 - col) * width + width / 2);
+    y = Math.round(board_y + (row - 1) * width + width / 2);
+  }
+  // console.log(square, "row", row, "col", col, x, y, "isblack", isBlack);
+
+  //   await page.evaluate(
+  //     (data) => {
+  //       console.log(data);
+  //       if (!window.debugDiv) {
+  //         window.debugDiv = document.createElement("div");
+  //         document.body.appendChild(window.debugDiv);
+  //       }
+
+  //       window.debugDiv.style.position = "fixed";
+  //       window.debugDiv.style.top = data.y - 40 + "px";
+  //       window.debugDiv.style.left = data.x - 40 + "px";
+  //       debugDiv.style.border = "1px solid red";
+  //       debugDiv.style.width = "80px";
+  //       debugDiv.style.height = "80px";
+  //     },
+  //     { x, y }
+  //   );
+
+  await clickon(page, fromSquare, 1000);
+  await delay(500);
+  await page.mouse.click(x, y);
+}
+async function isGameOver(page) {
+  const el = await page.$(".game-over-modal-content");
+  if (el) {
+    console.log("The game is over!");
+    return true;
+  }
+
+  return false;
+}
+async function playWithBestMove(page, callback) {
+  if (busy) return;
+  busy = true;
+
+  try {
+    const checkIfOver = await isGameOver(page);
+    if (checkIfOver) {
+      busy = false;
+      clearInterval(playIntervalId);
+      await callback();
+    }
+    const current_fen = await getCurrentFen(page);
+
+    if (current_fen && current_fen.includes("#")) {
+      console.log("The game is over!");
+      clearInterval(playIntervalId);
+      await callback();
+    }
+    // if (current_fen && !current_fen.includes("w")) {
+    //   console.log("Skip black move");
+    //   busy = false;
+    //   return;
+    // }
+
+    if ((current_fen && current_fen !== board_fen) || true) {
+      const best_move = await getBestMove(current_fen);
+      if (best_move.includes("none")) {
+        console.log("The game is over!");
+        clearInterval(playIntervalId);
+        await callback();
+        return;
+      }
+      console.log("best_move", best_move);
+      const from = best_move.slice(0, 2);
+      const to = best_move.slice(2, 4);
+      const fromSquare = "square-" + (from.charCodeAt(0) - 96) + from[1];
+      const toSquare = "square-" + (to.charCodeAt(0) - 96) + to[1];
+      console.log(from, to, fromSquare, toSquare);
+      const findEl = await page.$(".piece." + toSquare);
+
+      try {
+        if (findEl) {
+          await clickon(page, ".piece." + fromSquare, 1000);
+
+          await delay(100);
+
+          await clickon(page, ".piece." + toSquare, 1000);
+        } else {
+          await clickAt(
+            page,
+            to.charCodeAt(0) - 96 + to[1],
+            ".piece." + fromSquare
+          );
+        }
+
+        if (to[1] === "8") {
+          await clickon(page, "div.promotion-piece.wq", 500);
+        }
+        if (to[1] === "1") {
+          await clickon(page, "div.promotion-piece.bq", 500);
+        }
+
+        board_fen = current_fen;
+      } catch (err) {
+        try {
+          await clickAt(
+            page,
+            to.charCodeAt(0) - 96 + to[1],
+            ".piece." + fromSquare
+          );
+          // check if that is promotion
+          if (to[1] === "8") {
+            await clickon(page, "div.promotion-piece.wq", 500);
+          }
+          board_fen = current_fen;
+        } catch (err1) {
+          console.log(err1);
+        }
+        console.log(err);
+      }
+      // check if the game is over
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  busy = false;
+}
+async function clickon(page, el, timeout = 10000) {
+  console.log("click", el);
+  await page.waitForSelector(el, { timeout });
+  await page.click(el);
+}
+
+async function playWithHuman(page) {
+  await page.goto("https://www.chess.com/play/online");
+  await clickon(page, '[data-cy="new-game-time-selector-button"]', 2000);
+  await clickon(page, '[data-cy="time-selector-category-600"]', 2000);
+
+  await clickon(page, '[data-cy="new-game-index-play"]', 2000);
+  playIntervalId = setInterval(async () => {
+    await playWithBestMove(page, async () => {
+      // start the new game
+      await delay(5000);
+      busy = false;
+      await playWithHuman(page);
+    });
+  }, 1000);
+
+  // waiting for new game
+}
+
+const playWithComputer = async (page) => {
+  // await delay(5000);
+  await page.goto("https://www.chess.com/play/computer");
+  await closeModalFirst(page, 5000);
+  console.log("modal closed");
+
+  const testfen = await getCurrentFen(page);
+  console.log("existing game", testfen);
+  if (!testfen) {
+    await clickon(page, "button[title='Choose']", 2000);
+    // set mode as challenge
+    await clickon(page, "div[data-cy='Challenge']", 1000);
+    await clickon(page, "button[title='Play']", 1000);
+  }
+  console.log("start interval");
+  playIntervalId = setInterval(async () => {
+    await playWithBestMove(page, async () => {
+      // start the new game
+      await delay(5000);
+      busy = false;
+      await playWithComputer(page);
+    });
+  }, 1000);
+};
+
+async function main() {
+  const browser = await puppeteer.launch({
+    // devtools: true,
+    // executablePath:
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    userDataDir: "./data",
+    headless: false,
+    handleSIGTERM: true,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 1024 });
+
+  await ensureLogin(page);
+
+  console.log("Successfull login ");
+  const mode = process.argv[2];
+  if (mode === "computer") {
+    await playWithComputer(page);
+  }
+
+  if (mode === "human") {
+    await playWithHuman(page);
+  }
+}
+
+async function playOfflineGames(page) {
+  console.log("TBD");
+  // check if the offline game is available and play them
+  // after each game it will also start new offline game to keep the bot busy 24/7:)
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,16 @@
+## This project is for educational purposes
+
+## Introduction
+This project aims to build automation tools that place games on chess.com with the computer or other online people.  In the background, it will send a request to stockfish API 
+## Prerequisites
+- The stockfish API can be found at this repo: https://github.com/samuraitruong/stockfish-docker
+
+
+## Run  it 
+
+1. rename .env.sample to .env and update your username and password & stockfish API
+2. Play with human
+```sh
+    yarn && yarn start
+
+```
